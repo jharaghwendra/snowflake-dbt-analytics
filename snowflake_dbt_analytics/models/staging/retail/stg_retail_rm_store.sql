0347a718ed433ba2c446ac60f73daa50e5d792e2
R.Jha <jharaghwendra@gmail.com>
2026-06-18 21:28:27 +0200

    Updated staging and intermidate model

--- snowflake_dbt_analytics/models/staging/retail/stg_retail_rm_store.sql
+++ snowflake_dbt_analytics/models/staging/retail/stg_retail_rm_store.sql
@@ -1,3 +1,28 @@
+with source_data as (
+	select
+		store,
+		storetype,
+		assortment,
+		competitiondistance,
+		competitionopensincemonth,
+		competitionopensinceyear,
+		promo2,
+		promo2sinceweek,
+		promo2sinceyear,
+		promointerval
+	from {{ source('landing', 'retail_rm_store') }}
+)
+
 select
-*
-from {{ source('landing', 'retail_rm_store') }}
+	store as store_id,
+	storetype as store_type,
+	assortment,
+	competitiondistance as competition_distance_m,
+	competitionopensincemonth as competition_open_since_month,
+	competitionopensinceyear as competition_open_since_year,
+	promo2 as has_promo2,
+	promo2sinceweek as promo2_since_week,
+	promo2sinceyear as promo2_since_year,
+	promointerval as promo_interval,
+	current_timestamp() as last_updated_at
+from source_data
